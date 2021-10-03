3ecbe2a940c7c85bdafd42adc8b6762d12f612bf
yunyuyuan <326178275@qq.com>
2021-10-03 18:47:50 +0800

    build-out

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: '/data/www/wuziqi'
+  },
   resolve: {
     alias: {
       "socket.io-client": "socket.io-client/dist/socket.io.js",
